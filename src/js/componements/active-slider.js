c03883067c4570aea1804c5f7703b426b88e39f0
Egor <zotina1212@gmail.com>
2025-05-19 18:56:50 +0300

    Created Page BLock

--- src/js/componements/active-slider.js
+++ src/js/componements/active-slider.js
@@ -3,26 +3,26 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 
 export const useActiveSlider = () => {
-  new Swiper('.active__slider', {
-    slidesPerView: 'auto',
-    spaceBetween: 32,
-    loop: true,
-  });
+    new Swiper('.active__slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 32,
+        loop: true,
+    });
 };
 
 export const useClauseSlider = () => {
-  new Swiper('.clause__slider', {
-    modules: [Navigation, Autoplay],
-    slidesPerView: 3, 
-    spaceBetween: 33,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      prevEl: '.clause__btn--prev',
-      nextEl: '.clause__btn--next',
-    },
-  });
+    new Swiper('.clause__slider', {
+        modules: [Navigation, Autoplay],
+        slidesPerView: 3,
+        spaceBetween: 33,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            prevEl: '.clause__btn--prev',
+            nextEl: '.clause__btn--next',
+        },
+    });
 };
